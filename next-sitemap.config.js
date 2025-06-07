76ba950fb0dbf3ee1219c1591a559dacc96fd845
Kevon <kevon.nyles@gmail.com>
2025-06-07 11:48:10 -0400

    chore(seo): add robots.txt, next-sitemap, and SeoHead component for SEO improvements

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://myrentrange.com',
+  generateRobotsTxt: false, // using our manual robots.txt
+  sitemapSize: 5000,
+  changefreq: 'weekly',
+  priority: 0.7,
+}; 
