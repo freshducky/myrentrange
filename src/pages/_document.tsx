be10ed58469e71710fa0fcd1df3c7fb845fd5bfb
Kevon <kevon.nyles@gmail.com>
2025-06-10 02:51:37 -0400

    feat(ezoic): add Ezoic privacy/header scripts to <head> and scaffold ads.txt

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,19 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* Ezoic Privacy Scripts (must come first) */}
+          <script src="https://cmp.gatekeeperconsent.com/min.js" data-cfasync="false"></script>
+          <script src="https://the.gatekeeperconsent.com/cmp.min.js" data-cfasync="false"></script>
+          {/* Ezoic Header Script */}
+          <script async src="//www.ezojs.com/ezoic/sa.min.js"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.ezstandalone = window.ezstandalone || {};
+                ezstandalone.cmd = ezstandalone.cmd || [];
+              `,
+            }}
+          />
           <title>MyRentRange</title>
           {/* Google AdSense */}
           <script
